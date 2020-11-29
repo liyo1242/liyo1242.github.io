1407ba83ea0838746427b21e826ed88c10610344
liyo <liyo1242@gmail.com>
2020-11-30 07:57:47 +0800

    context add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useEffect } from 'react'
+import Context from './context'
 
-function App() {
+export default () => {
+  console.log('[Main Context Object Re-render]')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Context.Provider
 
-export default App;
+      >
+        {/* Routes */}
+      </Context.Provider>
+    </div>
+  )
+}
